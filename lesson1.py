5fdde2a2dca625366401f2a2ae0f1ba0cbc7dc35
Nam Chu <namhoangchu13@gmail.com>
2026-05-15 21:51:11 -0500

    lesson1

---
+++ lesson1.py
@@ -0,0 +1,16 @@
+#print(1,2,3,4,5,sep=" - ")
+
+#for i in range(1, 100):
+    #print(i, end=", ")
+#print(100)
+
+#a = list(range(1,101))
+#print(a)
+#print(", ".join(map(str, a)))
+
+#for i in range(1, 101):
+    #if i%2 == 0:
+        #print(i, end=", ")
+
+for i in range(2, 101, 2):
+    print(i, end=", ")
