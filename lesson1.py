c00965fcb85236252c1612395a9dd14864d5d55e
Nam Chu <namhoangchu13@gmail.com>
2026-05-22 22:51:03 -0500

    Update lesson1.py

--- lesson1.py
+++ lesson1.py
@@ -12,5 +12,16 @@
     #if i%2 == 0:
         #print(i, end=", ")
 
-for i in range(2, 101, 2):
-    print(i, end=", ")
+#for i in range(2, 101, 2):
+    #print(i, end=", ")
+#ten = "Nam"
+#tuoi = 13
+#diem_toan = 10
+#print("Tên của tồi là", ten)
+#print("Tồi", tuoi, "tuổi")
+#print("Điểm toán của tồi là", diem_toan)
+#print(type(False))
+#ten = input("Nhập tên của bạn là: ")
+#print ("Xin chào", ten)
+tuoi = int(input("Nhập tuồi của bạn là: "))
+print("Năm sau bạn sẽ là", tuoi + 1, "tuổi.")
